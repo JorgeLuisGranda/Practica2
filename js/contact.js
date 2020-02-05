eead5b6076a698e42176ccd3b1bd81842c3b2c2f
Jorge <jlgm97@hotmail.com>
2020-02-05 10:27:15 -0600

    cambio en h3

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+
+    (function($) {
+        "use strict";
+
+
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Favor de introducir tu nombre.",
+                    minlength: "Tu nombre debe contener más de dos caracteres."
+                },
+                subject: {
+                    required: "Introduce el asunto",
+                    minlength: "El asunto debe contener más de 4 caracteres"
+                },
+                number: {
+                    required: "Introduce tu número telefonico",
+                    minlength: "your Number must consist of at least 5 characters"
+                },
+                email: {
+                    required: "Ingresa tu e-mail"
+                },
+                message: {
+                    required: "Su mensaje necesita contenido.",
+                    minlength: "¿Desea agregar algo más?"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+
+ })(jQuery)
+})
